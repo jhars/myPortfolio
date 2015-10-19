c084ee654e4d93851cb5de32e050512496bf0b24
jhars <jjharlan06@gmail.com>
2015-10-19 02:49:49 -0700

    mobileDesign.html + mobileDesign.js

---
+++ js/mobileDesign.js
@@ -0,0 +1,57 @@
+var sources =  [
+                '../images/designGraphics/r7v7.gif',
+                '../images/designGraphics/rLogo.jpg',
+                '../images/designGraphics/rPackApp.png',
+                '../images/designGraphics/eSockMulti.jpg',
+                '../images/designGraphics/eSockBlkGr.jpg',
+                '../images/designGraphics/eSockOrange.jpg',
+                '../images/designGraphics/eTHLogo.png',
+                '../images/designGraphics/Camo-pocket.jpg',
+                '../images/designGraphics/AzteCPocket.png',             
+                '../images/designGraphics/rFLGS.jpg'
+                ];
+
+// var sources = [[BayViewGoes Here]]
+
+//============IMAGE LOADER=================//
+var images = {};
+
+function loadImages(sources, callback) {
+
+
+  var loadedImages = 0;
+  var numImages = 0;
+  // get num of sources
+  for(var i in sources) {
+    numImages++;
+    // src++;
+  }
+  for(var src in sources) {
+    images[src] = new Image();
+    images[src].onload = function() {
+      if(++loadedImages >= numImages) {
+        callback(images);
+      }
+
+    };
+    images[src].src = sources[src];
+  }
+}
+    
+  var canvas = document.getElementById('designDesign');
+  var context = canvas.getContext('2d');
+
+
+
+    loadImages(sources, function(images) {
+        
+      for (i=0;i< 12;i++) {//number of IMAGES Loaded
+        console.log(images[i]);
+        context.drawImage(images[i], 0, i*350, 350, 290);
+      context.shadowColor = '#999';
+      context.shadowBlur = 70;
+      context.shadowOffsetX = 0;
+      context.shadowOffsetY = 15;
+
+      }
+    });
